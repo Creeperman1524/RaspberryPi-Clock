8c808560baca31f755bc3521775084b57898e2b4
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2025-04-17 12:06:27 -0400

    Added linter
    Added eslint to lint the project and keep the code formatting consistent

--- MMM-TouchHandler.js
+++ MMM-TouchHandler.js
@@ -1,22 +1,22 @@
-Module.register("MMM-TouchHandler", {
-	notificationReceived: async function (noti, payload, sender) {
-		if(noti == "DOM_OBJECTS_CREATED") {
+Module.register('MMM-TouchHandler', {
+	notificationReceived: async function(noti, payload, sender) {
+		if (noti == 'DOM_OBJECTS_CREATED') {
 			document.body.onmousedown = () => {
-				this.sendNotification("SCENES_PREV");
+				this.sendNotification('SCENES_PREV');
 			};
 		}
 
 		// Handle the clock size changes
-		if(noti == "SCENES_CHANGED") {
+		if (noti == 'SCENES_CHANGED') {
 			payload.then((p) => {
-				if(p.currentScene.name != "Default") { 
-					document.querySelector(".clock").classList.remove('big');
-					document.querySelector(".clock").classList.add('small');
-				} else if (p.currentScene.name == "Default") {
-					document.querySelector(".clock").classList.remove('small');
-					document.querySelector(".clock").classList.add('big');
+				if (p.currentScene.name != 'Default') {
+					document.querySelector('.clock').classList.remove('big');
+					document.querySelector('.clock').classList.add('small');
+				} else if (p.currentScene.name == 'Default') {
+					document.querySelector('.clock').classList.remove('small');
+					document.querySelector('.clock').classList.add('big');
 				}
-			})
+			});
 		}
-	}
-})
+	},
+});
